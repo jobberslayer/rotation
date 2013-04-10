c16ec4fb0fdc9f3dce5cf4a1f03a5b186be351e4
Kevin Lester <kevin@e-kevin.com>
2013-04-10 16:40:57 -0400

    refactored to make cleaner

--- spec/requests/volunteers_spec.rb
+++ spec/requests/volunteers_spec.rb
@@ -7,5 +7,6 @@ describe "Volunteers" do
   describe "GET /volunteers" do
     before { get volunteers_url }
     it { response.status.should be(200) }
+    it { page.title.should eq full_title('Volunteers')}
   end
 end
