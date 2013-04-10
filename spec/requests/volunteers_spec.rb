068c29b3110679e4b94eea9e6d1b08828d851ec9
Kevin Lester <kevin@e-kevin.com>
2013-04-10 16:27:27 -0400

    refactored to make cleaner

--- spec/requests/volunteers_spec.rb
+++ spec/requests/volunteers_spec.rb
@@ -5,11 +5,7 @@ describe "Volunteers" do
   before { sign_in user }  
 
   describe "GET /volunteers" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      puts volunteers_url
-      get volunteers_url
-      response.status.should be(200)
-    end
+    before { get volunteers_url }
+    it { response.status.should be(200) }
   end
 end
