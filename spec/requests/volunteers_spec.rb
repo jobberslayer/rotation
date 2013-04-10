1986fe5a379d1f5a1fb3f72e4d7b76a433841985
Kevin Lester <kevin@e-kevin.com>
2013-04-10 17:11:39 -0400

    latest

--- spec/requests/volunteers_spec.rb
+++ spec/requests/volunteers_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
+include ApplicationHelper
 
 describe "Volunteers" do
   let(:user) { FactoryGirl.create(:user) }
   before { sign_in user }  
 
   describe "GET /volunteers" do
-    before { get volunteers_url }
-    it { response.status.should be(200) }
-    it { page.title.should eq full_title('Volunteers')}
+    before { visit volunteers_url }
+    it { page.status_code.should be(200) }
+    it { page.find('title').text.should eq full_title('Volunteers') }
   end
 end
