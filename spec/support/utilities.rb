965bbc20aad472b33e163bbd134735f92e8a7e31
Kevin Lester <kevin@e-kevin.com>
2013-04-12 18:12:49 -0400

    added have_success_message matcher

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -12,9 +12,15 @@ def sign_in(user)
   # Sign in when not using Capybara as well.
   cookies[:remember_token] = user.remember_token
 end
-
+  
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-success', text: message)
   end
 end
